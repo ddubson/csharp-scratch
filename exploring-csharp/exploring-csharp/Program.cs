59576a8e90a69cb5a5d75553d227cb61de44bd4d
Dmitriy Dubson <d.dubson@gmail.com>
2019-05-29 07:07:18 -0400

    Initial programs

---
+++ exploring-csharp/exploring-csharp/Program.cs
@@ -0,0 +1,43 @@
+﻿using Person;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exploring_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteStatement();
+            ExampleOfNamespace();
+        }
+
+        private static void ExampleOfNamespace()
+        {
+            TypicalPerson person = new TypicalPerson();
+            person.name = "You!";
+
+            Console.WriteLine(person.name);
+        }
+
+        private static void WriteStatement()
+        {
+            var FirstName = "James";
+            var LastName = "Bond";
+            var FullName = $"{FirstName} {LastName}"; // String Interpolation goodness in C#
+
+            Console.WriteLine($"My Name is {FullName}");
+        }
+    }
+}
+
+namespace Person
+{
+    class TypicalPerson
+    {
+        public String name { get; set; } 
+    }
+}
